62b02810a26031a7b735d22ffa29196754836fd9
Abhishek Bhattacharjee <abhishek.sandip@anywhere.co>
2023-07-11 21:15:49 +0530

    Date counter react example project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
+export default function App(){
+  const [step,setStep]=useState(1);
+  const [count,setCount]=useState(0);
+
+  const currentDate=new Date();
+  currentDate.setDate(currentDate.getDate()+count);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="container">
+      <div className="step">
+        <button onClick={()=>step>1 && setStep(step=>step-1)}>-</button>
+        <span> Steps : {step} </span>
+        <button onClick={()=>setStep(step=>step+1)}>+</button>
+      </div>
 
-export default App;
+      <div className="count">
+        <button onClick={()=>setCount(count=>count-step)}>-</button>
+        <span> Count : {count} </span>
+        <button onClick={()=>setCount(count=>count+step)}>+</button>
+      </div>
+
+      <div className="text-box">
+        <p>{count===0?`Today is `:count>0?`${count} days from today is `:`${Math.abs(count)} days ago was `}{currentDate.toDateString()}</p>
+      </div>
+    </div>
+  )
+}
